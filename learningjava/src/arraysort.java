115d2a6dcb5049d1c5b195e1960be0a32395a538
Nishant Kumar Singh <32234614+nishantnksingh@users.noreply.github.com>
2021-10-02 13:20:31 +0530

    Update arraysort.java

--- learningjava/src/arraysort.java
+++ learningjava/src/arraysort.java
@@ -4,15 +4,16 @@ import java.util.Arrays;
 public class arraysort {
     public static void main(String[] args) {
         ArrayList<Integer>  arr = new ArrayList<Integer>();
-        arr.add(20);
-        arr.add(30);
-        arr.add(10);
-        arr.add(50);
-        arr.add(40);
+        //The program should accept input from the user
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++){
+            arr.add(sc.nextInt());
+        }
       Object[] a = arr.toArray();
         Arrays.sort(a);
         for(int i=0; i<5;i++){
-            System.out.println(a[i]);
+            System.out.println(a[i]+" ");
         }
 
 
